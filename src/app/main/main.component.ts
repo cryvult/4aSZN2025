e943f8c6015ef2b54f8c4828bb1d4a36fb9287fd
Artur <artur.krywult17@gmail.com>
2025-05-21 13:58:17 +0200

    komit2

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { NgModel } from '@angular/forms';
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [NgModel,FormsModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+  inputText: string = ''; 
+  displayedText: string = ''; 
 
+  // Funkcja wywoływana po kliknięciu przycisku
+  addText() {
+    this.displayedText = this.inputText; 
+    this.inputText = ''; 
+  }
 }
